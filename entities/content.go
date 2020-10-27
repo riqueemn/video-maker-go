c241e8bebd401c7bd4213368071addf9209c4bd4
Henrique Moreira <henriquemoreira842@gmail.com>
2020-10-27 02:04:02 -0300

    Add robots: state and input

--- entities/content.go
+++ entities/content.go
@@ -2,9 +2,9 @@ package entities
 
 //Content -> Conteúdo da pesquisa
 type Content struct {
-	SearchTerm             string
-	PrefixName             string
-	SourceContentOriginal  string
-	SourceContentSanitized string
-	Sentences              []Sentence
+	SearchTerm             string     `json:"SearchTerm"`
+	PrefixName             string     `json:"PrefixName"`
+	SourceContentOriginal  string     `json:"SourceContentOriginal"`
+	SourceContentSanitized string     `json:"SourceContentSanitized"`
+	Sentences              []Sentence `json:"Sentences"`
 }
--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 
 	"github.com/riqueemn/video-maker-go/entities"
 	"github.com/riqueemn/video-maker-go/robots"
@@ -13,79 +10,17 @@ import (
 const ()
 
 var (
-	content   entities.Content
-	robotText robots.Text
+	//content    entities.Content
+	robotState robots.State
+	robotInput robots.Input
+	robotText  robots.Text
 )
 
-func askAndReturnSearchTerm() string {
-	fmt.Print("\nType a Wikipedia shearch Term: ")
-	//fmt.Println("Digite um termo para pesquisa do Wikipedia:")
-
-	//reader := bufio.NewReader(os.Stdin)
-	//text, _ := reader.ReadString('\n')
-
-	//fmt.Println(text)
-	var text string
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for scanner.Scan() {
-		text = scanner.Text()
-		return text
-	}
-
-	return ""
-}
-
-func askAndReturnPrefix() string {
-	prefixes := []string{"Who is", "What is", "The history of", "Cancel"}
-	//const prefixes = ["Quem é", "O que é", "A história de", "Cancelar"]
-
-	//reader := bufio.NewReader(os.Stdin)
-	//text, _ := reader.ReadString('\n')
-	//log.Println(text)
-	//n, _ := strconv.Atoi(text)
-	//log.Println(n)
-	//n, _ := strconv.Atoi(text)
-
-	for i, prefixe := range prefixes {
-		if i != len(prefixes)-1 {
-			fmt.Print("\n[", i+1, "] ", prefixe)
-
-			continue
-		}
-		fmt.Print("\n[", 0, "] ", prefixe)
-
-	}
-	fmt.Print("\n\n")
-
-	var text string
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for scanner.Scan() {
-		text = scanner.Text()
-		if text != "1" && text != "2" && text != "3" && text != "0" {
-			fmt.Println("Valor indevido!!!")
-			os.Exit(1)
-		} else {
-			if text == "0" {
-				os.Exit(1)
-			}
-
-			n, _ := strconv.Atoi(text)
-
-			return prefixes[n-1]
-		}
-	}
-
-	return ""
-}
-
 func main() {
+	robotInput.RobotProcess()
+	robotText.RobotProcess()
 
-	content.SearchTerm = askAndReturnSearchTerm()
-	content.PrefixName = askAndReturnPrefix()
-
-	robotText.RobotProcess(&content)
+	content := robotState.Load()
 
 	fmt.Print("\n")
 	print(content)
